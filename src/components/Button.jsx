877cc8749ea2d5cb52eb918debac3e43cbd56120
Norman Leyva Garcia <75862898+nlgarcia84@users.noreply.github.com>
2026-02-10 19:45:47 +0100

    Problemas de react corregidos

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const Button = ({ buttonTitle, onClick }) => {
   return (
     <>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Button } from './Button';
 
 export const Card = ({ title, data, onClick }) => {
