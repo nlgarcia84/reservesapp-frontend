74c20b71badead252a7a43b4066ee68100623d61
Norman Leyva Garcia <75862898+nlgarcia84@users.noreply.github.com>
2026-02-23 23:53:49 +0100

    login edited

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,8 +3,6 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { login } from '@/app/services/auth';
-import Image from 'next/image';
-import logo from '../../public/logo_reservesapp.png';
 
 const LoginPage = () => {
   const router = useRouter();
@@ -31,42 +29,37 @@ const LoginPage = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-slate-950">
-      <div className="flex flex-col items-center pb-10 mb-5">
-        <Image
-          src={logo}
-          alt="logo de ReservesApp"
-          width={150}
-          height={150}
-          loading="eager"
-        />
-        <span
-          className="text-emerald-400 font-bold text-4xl mb-2
+      <div className="flex flex-row items-center">
+        <div className="flex flex-col text-center mb-15">
+          <span
+            className="text-emerald-400 font-bold text-5xl mb-2
              [text-shadow:0_0_25px_rgba(52,211,153,0.55)]"
-        >
-          ReservesApp
-        </span>
-        <span className="text-slate-300 font-light">
-          Gestió de sales i reserves
-        </span>
+          >
+            ReservesApp
+          </span>
+          <span className="text-slate-300 font-extralight text-xl">
+            Gestió de sales i reserves
+          </span>
+        </div>
       </div>
 
       <form
         onSubmit={handleSubmit}
         className="border border-emerald-600 rounded-2xl ring-2 ring-emerald-400/30
-             shadow-[0_0_25px_rgba(52,211,153,0.35)] p-10 w-full px-4 sm:px-6 md:px-8 max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl"
+             shadow-[0_0_25px_rgba(52,211,153,0.35)]  sm:py-15 sm:px-1 w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl"
       >
-        <div className="text-center mb-5">
-          <p className="text-white text-xl sm:text-4xl mb-2 text-center font-bold">
+        <div className="text-center mb-5 p-5">
+          <p className="text-white text-3xl md:text-xl mb-2 text-center font-bold">
             Inicia sessió
           </p>
-          <p className="text-slate-300 font-light">
+          <p className="text-slate-300 text-lg font-light">
             Introdueix les teves credencials per accedir
           </p>
         </div>
 
         {error && <p className="text-red-500 mb-4">{error}</p>}
 
-        <div className="p-2">
+        <div className="p-5">
           <div className="w-full md:w-4/5 lg:w-2/3 mx-auto">
             <p className="font-bold pb-2">Correu electrònic</p>
             <input
