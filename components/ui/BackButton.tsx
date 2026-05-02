2326bd12fcc9ab423a4c851a8ac4a583047ac617
Norman Leyva Garcia <75862898+nlgarcia84@users.noreply.github.com>
2026-05-02 12:17:07 +0200

    implementada logica de enrutado boton BackButton condicionada a si hay o no reservas

--- components/ui/BackButton.tsx
+++ components/ui/BackButton.tsx
@@ -1,19 +1,41 @@
-import Link from 'next/link';
 import { SquareChevronLeft } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 type BackButtonProps = {
-  previouspage: string;
+  previouspage?: string;
+  conditionalNav?: () => void;
 };
 
-export const BackButton = ({ previouspage }: BackButtonProps) => {
+export const BackButton = ({
+  previouspage,
+  conditionalNav,
+}: BackButtonProps) => {
+  const router = useRouter();
+
+  const handlePress = () => {
+    // 1. Si el usuario pasó una función lógica, la ejecutamos
+    if (conditionalNav) {
+      conditionalNav();
+    }
+    // 2. Si no hay función pero hay una ruta fija, vamos a ella
+    else if (previouspage) {
+      router.push(previouspage);
+    }
+    // 3. Fallback: si no hay nada, simplemente volver atrás en el historial
+    else {
+      router.back();
+    }
+  };
+
   return (
     <>
-      <Link href={previouspage}>
-        <div className="flex flex-row justify-center text-blue-500 font-semibold hover:text-blue-400 cursor-pointer">
-          <SquareChevronLeft />
-          <span className="pl-2">Tornar</span>
-        </div>
-      </Link>
+      <button
+        onClick={handlePress}
+        className="flex flex-row items-center justify-center text-blue-500 font-semibold hover:text-blue-400 cursor-pointer bg-transparent border-none outline-none"
+      >
+        <SquareChevronLeft />
+        <span className="pl-2">Tornar</span>
+      </button>
     </>
   );
 };
